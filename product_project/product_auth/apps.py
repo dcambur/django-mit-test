a7fbb1418d64c9db89e36d64114f9184a8b6093d
dcambur <dcambur@gmail.com>
2024-11-11 12:37:29 +0200

    add fields and auto-create user profile

--- product_project/product_auth/apps.py
+++ product_project/product_auth/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product_auth'
+
+    def ready(self):
+        import product_auth.signals
--- product_project/product_auth/urls.py
+++ product_project/product_auth/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", SignUp.as_view(), name="sign_up"),
-    path("user/<int:pk>/", UserProfileView.as_view(), name="user_profile")
+    path("", SignUpCreate.as_view(), name="sign_up"),
+    path("user/<int:pk>/", UserProfileDetail.as_view(), name="user_profile_detail"),
+    path("user/<int:pk>/update", UserProfileUpdate.as_view(), name="user_profile_update")
 ]
